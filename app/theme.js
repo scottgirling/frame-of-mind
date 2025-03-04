3952b383dda293ef7cb4e83dcec32a9d43dea940
CaptainHarrie <butchlinkle@gmail.com>
2025-03-04 23:03:08 +0000

    add more theme colours

--- app/theme.js
+++ app/theme.js
@@ -6,8 +6,13 @@ const theme = createTheme({
     primary: {
       main: "#6750A4",
       light: "#EADDFF",
+      medium: "#D0BCFF",
+      emphasis: "#9C85D1",
       dark: "#4F378B",
       contrastText: "#FFFFFF",
+    },
+    light: {
+      main: "#FEF7FF",
     },
   },
 });
