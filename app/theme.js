ffadb648da0e95b198e05b1383e9e8716fdd7938
CaptainHarrie <butchlinkle@gmail.com>
2025-03-04 19:55:11 +0000

    create base theme and layout

---
+++ app/theme.js
@@ -0,0 +1,15 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#6750A4",
+      light: "#EADDFF",
+      dark: "#4F378B",
+      contrastText: "#FFFFFF",
+    },
+  },
+});
+
+export default theme;
