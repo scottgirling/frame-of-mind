099c4389d0553feaa45c55cd71a8b78bcfb00081
CaptainHarrie <butchlinkle@gmail.com>
2025-03-06 12:50:17 +0000

    disable drop shadows

--- app/theme.js
+++ app/theme.js
@@ -1,7 +1,12 @@
 "use client";
 import { createTheme } from "@mui/material/styles";
+// create a temporary theme to get the default options
+const defaultTheme = createTheme();
 
+// get the default `shadows` object
+const defaultShadows = [...defaultTheme.shadows];
 const theme = createTheme({
+  shadows: defaultShadows.map(() => "none"),
   palette: {
     primary: {
       main: "#6750A4",
