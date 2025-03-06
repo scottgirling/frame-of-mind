1ab7f4fdd3eebbb7e3b4942e21e03e6e93ccbcd9
CaptainHarrie <butchlinkle@gmail.com>
2025-03-06 13:28:15 +0000

    add testing navigation

--- app/(standard)/layout.js
+++ app/(standard)/layout.js
@@ -1,13 +1,27 @@
-import { Box } from "@mui/material";
+import { Box, Button, ButtonGroup, Typography } from "@mui/material";
 import TopBar from "../components/TopBar";
 import BottomBar from "../components/BottomBar";
+import Avatar from "../components/Avatar";
 
 export default function StandardLayout({ children }) {
   return (
     <>
-      <TopBar />
+      <TopBar components={<Avatar sx={{ ml: "auto" }} />} />
       <Box component={"main"} sx={{ mx: "auto", mt: "1.25rem" }}>
         <section>{children}</section>
+        <section>
+          <Typography variant="h6" sx={{ mt: 5, mb: 1 }}>
+            Go To:
+          </Typography>
+          <ButtonGroup>
+            <Button href="/setup" variant="outlined">
+              Create Comic
+            </Button>
+            <Button href="/create" variant="outlined">
+              Drawing Canvas
+            </Button>
+          </ButtonGroup>
+        </section>
       </Box>
       <BottomBar />
     </>
