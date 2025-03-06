f1be1e64c6fbd3368d99aa9ad74d25d840cddc26
CaptainHarrie <butchlinkle@gmail.com>
2025-03-06 12:41:16 +0000

    update topbar and logo components

--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -1,3 +1,11 @@
 export default function Logo() {
-  return <img src="/assets/logo.png" style={{ width: "2.5rem" }} />;
+  return (
+    <img
+      src="/assets/logo.png"
+      style={{
+        width: "2.5rem",
+        display: "inline-block",
+      }}
+    />
+  );
 }
