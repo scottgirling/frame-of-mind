55f7e559ed0c8bf4d5a9432a2f6abd10bfbaf8ed
CaptainHarrie <butchlinkle@gmail.com>
2025-03-07 15:08:09 +0000

    (Holly Skinner) set up the setup page

--- app/(standard)/(home)/setup/page.js
+++ app/(standard)/(home)/setup/page.js
@@ -1,3 +1,42 @@
+"use client";
+
+import { Box, Button, Switch } from "@mui/material";
+import { useState } from "react";
+import { useCollectionOnce } from "react-firebase-hooks/firestore";
+
 export default function CreateComicPage() {
-  return <p>Create page</p>;
+  const [isSolo, setIsSolo] = useState(true);
+  const uid = "YNy86Pz4AY1Mtweqx1hn"; // Fake user id
+
+  function continueExistingSolo(uid) {
+    // const [snapshot, loading, error] = useCollectionOnce(query);
+    // const existingComics
+    // return existingComics
+  }
+
+  return (
+    <Box component={"section"}>
+      <p>Choose your game-play modes below!</p>
+      <Box>
+        Solo
+        <Switch
+          onClick={() => {
+            setIsSolo(!isSolo);
+          }}
+        />
+        Team
+      </Box>
+      <Box>
+        <Button
+          variant="contained"
+          onClick={() => {
+            continueExistingSolo(uid);
+          }}
+        >
+          Continue existing comic
+        </Button>
+        <Button variant="contained">New comic</Button>
+      </Box>
+    </Box>
+  );
 }
