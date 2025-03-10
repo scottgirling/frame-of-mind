3b46317dc4103c774cb2e5e92c62064038d1c529
CaptainHarrie <butchlinkle@gmail.com>
2025-03-10 14:03:49 +0000

    delete comic if removing last panel

---
+++ app/(standard)/(home)/create/utils/deletePanel.js
@@ -0,0 +1,32 @@
+import { db } from "@/lib/firebase";
+import {
+  doc,
+  deleteDoc,
+  updateDoc,
+  arrayRemove,
+  getDoc,
+} from "firebase/firestore";
+
+export default async function deletePanel(uid, comicId, panelId) {
+  try {
+    // Delete panel from panels collection
+    const panelRef = doc(db, "panels", panelId);
+    await deleteDoc(panelRef);
+    console.log(`Panel ${panelId} deleted from panels collection.`);
+
+    // 2. Delete panel from panels array in the comics doc
+    const comicRef = doc(db, "comics", comicId);
+    const comicSnapshot = await getDoc(comicRef);
+    const panelsCount = comicSnapshot.data().panels.length;
+
+    if (panelsCount === 1) {
+      await deleteDoc(comicRef);
+    } else {
+      await updateDoc(comicRef, {
+        panels: arrayRemove(panelRef),
+      });
+    }
+  } catch (error) {
+    console.error("Error deleting panel:", error);
+  }
+}
