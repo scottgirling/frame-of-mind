097c1991d14fe00e8c72296e62f9f6cb18dd00b8
Holly Skinner <hollyskinner97@gmail.com>
2025-03-10 16:13:29 +0000

    set up week streak component

---
+++ app/components/streak/WeekStreak.jsx
@@ -0,0 +1,15 @@
+import { Box, Typography } from "@mui/material";
+
+export default function WeekStreak({ weekStreak, dayStreak }) {
+  return (
+    <Box sx={{ bgcolor: "primary.light", borderRadius: 3, p: 5 }}>
+      <Typography sx={{}}>Your week streak: {weekStreak}</Typography>
+      <Typography>
+        {weekStreak > 1 ? "You’re on fire! Keep it up!" : "Welcome back!"}
+        {dayStreak > 1
+          ? " Draw something every day to maintain your streak!"
+          : " Draw something today to start your streak!"}
+      </Typography>
+    </Box>
+  );
+}
