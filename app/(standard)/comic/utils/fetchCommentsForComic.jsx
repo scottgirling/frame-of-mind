fb8e03a97234ed27e72839d07af1dfd2db666bc3
Holly Skinner <hollyskinner97@gmail.com>
2025-03-11 21:09:20 +0000

    create util to fetch the comments for a specific comic

---
+++ app/(standard)/comic/utils/fetchCommentsForComic.jsx
@@ -0,0 +1,25 @@
+import { db } from "@/lib/firebase";
+import {
+  doc,
+  query,
+  getDocs,
+  collection,
+  where,
+  orderBy,
+} from "firebase/firestore";
+
+export default async function fetchCommentsForComic(comicRef) {
+  // Return comments related to the comic passed in
+  const q = query(
+    collection(db, "comments"),
+    where("comicRef", "==", comicRef),
+    orderBy("createdAt", "desc")
+  );
+  const querySnapshot = await getDocs(q);
+  const comments = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  return comments;
+}
