264e78d16ba14fd0eb5e9b60b9e6f263b72954d4
CaptainHarrie <butchlinkle@gmail.com>
2025-03-11 11:20:26 +0000

    set defaults if values do not exist, otherwise use existing values

--- app/auth/utils/handleGoogleLogin.js
+++ app/auth/utils/handleGoogleLogin.js
@@ -7,13 +7,19 @@ export default async function handleGoogleLogIn() {
     const result = await signInWithPopup(auth, provider);
     const user = result.user;
     const userRef = doc(db, "users", user.uid);
+    const userInfo = await getData("users", userRef);
 
     await setDoc(userRef, {
       displayName: user.displayName,
       email: user.email,
       avatarUrl: user.providerData[0].photoURL ?? "",
+      dayStreak: userInfo.dayStreak ?? 0,
+      weekStreak: userInfo.weekStreak ?? 0,
+      userBio: userInfo.userBio ?? "",
+      points: userInfo.points ?? 0,
       createdAt: user.metadata.creationTime,
       lastSignInTime: user.metadata.lastSignInTime,
+      myComics: userInfo.myComics ?? [],
     });
   } catch (error) {
     console.log(error);
