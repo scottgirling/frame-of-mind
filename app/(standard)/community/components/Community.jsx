40a500f12083ac679cfcdcd9b8d349aa2a7e7da3
Kyle <email@kylenoad.com>
2025-03-12 14:53:12 +0000

    started to work on layout for community page

--- app/(standard)/community/components/Community.jsx
+++ app/(standard)/community/components/Community.jsx
@@ -3,10 +3,12 @@ import { useEffect, useState } from "react";
 import fetchCompletedComics from "../utils/fetchCompletedComics";
 import fetchComicPanel from "../utils/fetchComicPanel";
 
-import { Button, ButtonGroup, Typography } from "@mui/material";
+import { Button, ButtonGroup, Typography, Grid2 } from "@mui/material";
 
 export default function Community() {
   const [comics, setComics] = useState([]);
+
+  console.log(comics, "<======COMICS");
 
   useEffect(() => {
     async function fetchData() {
@@ -35,10 +37,20 @@ export default function Community() {
         </Button>
       </ButtonGroup>
 
-      <Typography variant="body1" sx={{ textAlign: "center" }}>
+      <Typography variant="h3" sx={{ textAlign: "center" }}>
         Community
       </Typography>
-      <div>
+      <Typography variant="body1" sx={{ textAlign: "center" }}>
+        See what other users have created!
+      </Typography>
+
+      <Grid2
+        container
+        spacing={3}
+        sx={{
+          justifyContent: "center",
+        }}
+      >
         {comics.map((comic) => (
           <div key={comic.id}>
             <img src={comic.firstPanelImage} alt="" />
@@ -46,9 +58,10 @@ export default function Community() {
             <Typography variant="body2">
               Created: {comic.createdAt.toDate().toLocaleDateString()}
             </Typography>
+            <Typography variant="body2">Likes: {comic.comicLikes}</Typography>
           </div>
         ))}
-      </div>
+      </Grid2>
     </>
   );
 }
