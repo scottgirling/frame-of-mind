69e2d23d6ffef64b0efce22344a0770fb52bcd1f
Holly Skinner <hollyskinner97@gmail.com>
2025-03-13 00:23:09 +0000

    add game description

--- app/(standard)/(home)/components/Home.jsx
+++ app/(standard)/(home)/components/Home.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Box, Grid2 as Grid, Typography } from "@mui/material";
 import Streaks from "@/app/components/streak/Streaks";
 import PaperButton from "@/app/components/PaperButton";
@@ -6,6 +8,13 @@ import PaperBox from "@/app/components/PaperBox";
 import { Palette } from "@phosphor-icons/react";
 
 export default function HomePage({ user }) {
+  const gameDescription =
+    "Frame of Mind is a creative storytelling app that helps you express your emotions through art. Draw solo or collaborate with others to improve your mental well-being a litle each day.\n\n" +
+    "Features include:\n" +
+    "- Virtual Art Studio: Earn points and collect items to inspire your creativity.\n" +
+    "- Community Hub: Share comics, and connect with others.\n\n" +
+    "Your imagination deserves a safe space — we hope to see you again tomorrow!";
+
   return (
     <>
       <Typography
@@ -38,18 +47,20 @@ export default function HomePage({ user }) {
         </PaperButton>
       </Box>
       <Grid container spacing={2}>
-        <Grid size={{ md: 6 }}>
+        <Grid size={{ md: 5.5 }}>
           <Streaks user={user} />
         </Grid>
-        <Grid size={{ md: 6 }}>
+        <Grid size={{ md: 6.5 }}>
           <PaperBox
             colour="primary"
             variant="light"
-            sx={{ p: 5, height: "100%" }}
+            sx={{ p: 3.5, height: "100%" }}
             margin={{ height: "100%" }}
             rotation={-1}
           >
-            <Typography>Lorem ipsum...</Typography>
+            <Typography sx={{ fontSize: "1rem", whiteSpace: "pre-line" }}>
+              {gameDescription}
+            </Typography>
           </PaperBox>
         </Grid>
       </Grid>
